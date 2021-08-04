b088c60da308529df2b63e69d4158055fb452f88
Evgeniy Moroz <jeksifrost@gmail.com>
2021-08-04 16:28:26 +0300

    Changing secret key

--- test_project_1/settings.py
+++ test_project_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2e@m4c+58a=j1)u8bg0_h@55d4c4n&1v=+ob7=4c--&*oh4tz7'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
